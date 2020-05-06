394f29defe988c16c9f108443baffbdd90b9b83b
Fabrice MUT <fabrice@groweb.fr>
2020-05-06 11:18:21 +0200

    modifs

--- screens/signup/index.js
+++ screens/signup/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   Container,
-  Header,
   Content,
   Form,
   Item,
@@ -12,9 +11,11 @@ import {
   Text,
   Icon,
   H1,
+  Toast,
+  Root,
 } from "native-base";
+import { StyleSheet, ScrollView, StatusBar } from "react-native";
 import { signUpUser } from "../../services/authentification";
-import validation from "../../validation";
 import validate from "../../validation_wrapper";
 import { COLORS } from "../../styles/colors";
 
@@ -32,7 +33,7 @@ export default class SignUp extends React.Component {
     };
   }
 
-  handlePasswordInput() {
+  showPasswordInput() {
     this.setState((prevState) => ({
       iconPassword: prevState.iconPassword === "eye" ? "eye-off" : "eye",
       displayPassword: !prevState.displayPassword,
@@ -54,179 +55,210 @@ export default class SignUp extends React.Component {
     });
 
     if (!emailError && !passwordError) {
-      alert("Details are valid!");
-      /*signUpUser(this.state.email, this.state.password);*/
+      signUpUser(this.state.email, this.state.password);
     }
+  }
+
+  toastShow(error) {
+    console.log(error);
+    Toast.show({
+      text: error,
+      buttonText: "Okay",
+      buttonTextStyle: { color: "#008000" },
+      buttonStyle: { backgroundColor: "#5cb85c" },
+    });
   }
 
   render() {
     return (
-      <Container style={{ flex: 1 }}>
-        <View
-          style={{
+      <Root>
+        <ScrollView
+          contentContainerStyle={{
             flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: COLORS.primary,
           }}
+          scrollEnabled
         >
-          <H1 style={{ color: "#FFF" }}>S'enregistrer</H1>
-        </View>
-        <View style={{ flex: 3 }}>
-          <Content
-            contentContainerStyle={{
+          <StatusBar backgroundColor={COLORS.primary_dark} />
+          <Container
+            style={{
               flex: 1,
-              flexDirection: "row",
-              justifyContent: "center",
+              backgroundColor: COLORS.primary_dark,
             }}
           >
-            <View style={{ flex: 0.8 }}>
-              <View>
-                <Form>
-                  <Item
-                    floatingLabel
-                    error={this.state.emailError ? true : false}
-                  >
-                    <Label>Email</Label>
-                    <Input
-                      onChangeText={(val) =>
-                        this.onChangeFormVal("email", val.trim())
-                      }
-                      placeholder={this.state.emailError}
-                      onBlur={() => {
-                        this.setState({
-                          emailError: validate("email", this.state.email),
-                        });
-                      }}
-                    />
-                    {this.state.emailError ? (
-                      <Icon name="close-circle" />
-                    ) : null}
-                  </Item>
-
-                  {this.state.emailError ? (
-                    <Text style={{ color: "red" }}>
-                      {this.state.emailError}
-                    </Text>
-                  ) : null}
-
-                  <Item
-                    floatingLabel
-                    error={this.state.passwordError ? true : false}
-                  >
-                    <Label>Mot de passe</Label>
-                    <Input
-                      secureTextEntry={this.state.displayPassword}
-                      error={this.state.passwordError}
-                      onChangeText={(val) =>
-                        this.onChangeFormVal("password", val.trim())
-                      }
-                      onBlur={() => {
-                        this.setState({
-                          passwordError: validate(
-                            "password",
-                            this.state.password
-                          ),
-                        });
-                      }}
-                    />
-                    {this.state.passwordError ? (
-                      <Icon name="close-circle" />
-                    ) : (
-                      <Icon
-                        name={this.state.iconPassword}
-                        onPress={() => this.handlePasswordInput()}
-                      />
-                    )}
-                  </Item>
-                  {this.state.passwordError ? (
-                    <Text style={{ color: "red" }}>
-                      {this.state.passwordError}
-                    </Text>
-                  ) : null}
-                </Form>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    justifyContent: "center",
-                    paddingTop: 10,
-                    paddingBottom: 10,
-                  }}
-                >
-                  <Text>Déjà un compte ? </Text>
-                  <Text
-                    style={{ color: COLORS.primary }}
-                    onPress={() => this.props.navigation.navigate("SignIn")}
-                  >
-                    Se connecter
-                  </Text>
-                </View>
-                <Button
-                  block
-                  rounded
-                  style={{ backgroundColor: COLORS.primary }}
-                  onPress={() => {
-                    this.handleSignUpForm();
-                  }}
-                >
-                  <Text>Inscription</Text>
-                </Button>
-                <View
-                  style={{
-                    paddingTop: 20,
-                    marginTop: 20,
-                    borderStyle: "solid",
-                    borderTopWidth: 1,
-                    borderTopColor: "#EDEDED",
-                  }}
-                >
-                  <View
-                    style={{
-                      alignItems: "space-between",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Button
-                      block
-                      rounded
-                      iconLeft
-                      style={{ backgroundColor: COLORS.facebookColor }}
-                      onPress={() => {
-                        this.signInWithFacebook();
-                      }}
-                    >
-                      <Icon name="logo-facebook" />
-                      <Text>S'enregistrer avec Facebook</Text>
-                    </Button>
-                  </View>
-                  <View
-                    style={{
-                      alignItems: "space-between",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Button
-                      block
-                      rounded
-                      iconLeft
-                      style={{
-                        backgroundColor: COLORS.googleColor,
-                        marginTop: 20,
-                      }}
-                      onPress={() => {
-                        this.signInWithGoogle();
-                      }}
-                    >
-                      <Icon name="logo-google" />
-                      <Text>S'enregistrer avec Google</Text>
-                    </Button>
-                  </View>
-                </View>
-              </View>
+            <View
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <H1 style={{ color: "#FFF" }}>Créer un compte</H1>
             </View>
-          </Content>
-        </View>
-      </Container>
+            <View
+              style={{
+                flex: 2,
+                backgroundColor: "#FFF",
+                borderTopLeftRadius: 50,
+                borderTopRightRadius: 50,
+              }}
+            >
+              <Content
+                contentContainerStyle={{
+                  flex: 1,
+                  flexDirection: "row",
+                  justifyContent: "center",
+                }}
+              >
+                <View style={{ flex: 0.8 }}>
+                  <Form>
+                    <Item
+                      floatingLabel
+                      error={this.state.emailError ? true : false}
+                    >
+                      <Label>Email</Label>
+                      <Input
+                        onChangeText={(val) =>
+                          this.onChangeFormVal("email", val.trim())
+                        }
+                        placeholder={this.state.emailError}
+                      />
+                      {this.state.emailError ? (
+                        <Icon name="close-circle" />
+                      ) : null}
+                    </Item>
+
+                    {this.state.emailError ? (
+                      <Text style={{ color: "red", fontSize: 12 }}>
+                        {this.state.emailError}
+                      </Text>
+                    ) : null}
+
+                    <Item
+                      floatingLabel
+                      error={this.state.passwordError ? true : false}
+                    >
+                      <Label>Mot de passe</Label>
+                      <Input
+                        secureTextEntry={this.state.displayPassword}
+                        error={this.state.passwordError}
+                        onChangeText={(val) =>
+                          this.onChangeFormVal("password", val.trim())
+                        }
+                      />
+                      {this.state.passwordError ? (
+                        <Icon name="close-circle" />
+                      ) : (
+                        <Icon
+                          name={this.state.iconPassword}
+                          onPress={() => this.showPasswordInput()}
+                        />
+                      )}
+                    </Item>
+                    {this.state.passwordError ? (
+                      <Text
+                        style={{
+                          color: "red",
+                          fontSize: 12,
+                        }}
+                      >
+                        {this.state.passwordError}
+                      </Text>
+                    ) : null}
+                  </Form>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "center",
+                      paddingTop: 10,
+                      paddingBottom: 10,
+                    }}
+                  >
+                    <Text>Déjà un compte ? </Text>
+                    <Text
+                      style={{ color: COLORS.primary_dark }}
+                      onPress={() => this.props.navigation.navigate("SignIn")}
+                    >
+                      Se connecter
+                    </Text>
+                  </View>
+                  <Button
+                    block
+                    rounded
+                    style={{
+                      backgroundColor: COLORS.primary_dark,
+                    }}
+                    onPress={() => {
+                      this.handleSignUpForm();
+                    }}
+                  >
+                    <Text>Créer un compte</Text>
+                  </Button>
+                  <View style={style.socialContainer}>
+                    <View
+                      style={{
+                        alignItems: "space-between",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <Button
+                        block
+                        rounded
+                        iconLeft
+                        style={style.facebookButton}
+                        onPress={() => {
+                          this.signInWithFacebook();
+                        }}
+                      >
+                        <Icon name="logo-facebook" />
+                        <Text>Se connecter avec Facebook</Text>
+                      </Button>
+                    </View>
+                    <View
+                      style={{
+                        alignItems: "space-between",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <Button
+                        block
+                        rounded
+                        iconLeft
+                        style={style.googleButton}
+                        onPress={() => {
+                          this.signInWithGoogle();
+                        }}
+                      >
+                        <Icon name="logo-google" />
+                        <Text>Se connecter avec Google</Text>
+                      </Button>
+                    </View>
+                  </View>
+                </View>
+              </Content>
+            </View>
+          </Container>
+        </ScrollView>
+      </Root>
     );
   }
 }
+
+const style = StyleSheet.create({
+  googleButton: {
+    backgroundColor: COLORS.googleColor,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  facebookButton: {
+    backgroundColor: COLORS.facebookColor,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  socialContainer: {
+    paddingTop: 20,
+    marginTop: 20,
+    borderStyle: "solid",
+    borderTopWidth: 1,
+    borderTopColor: "#EDEDED",
+  },
+});
--- services/authentification/index.js
+++ services/authentification/index.js
@@ -1,5 +1,10 @@
 import * as firebase from "firebase";
 const Auth = firebase.auth();
+import { Toast } from "native-base";
+
+export const errorMessageFR = {
+  "auth/email-already-in-use": "Cet email existe déjà",
+};
 
 export const signInUser = (email, password) => {
   Auth.signInWithEmailAndPassword(email, password).catch(function (error) {
@@ -7,16 +12,33 @@ export const signInUser = (email, password) => {
     var errorCode = error.code;
     var errorMessage = error.message;
 
-    console.log(errorCode, errorMessage);
+    if (this.erroMessage) console.log(errorCode, errorMessage);
     // ...
   });
 };
 
-export const signUpUser = () => {
+export const signUpUser = (email, password) => {
   Auth.createUserWithEmailAndPassword(email, password).catch(function (error) {
     // Handle Errors here.
     var errorCode = error.code;
-    var errorMessage = error.message;
+    let errorMessage = errorMessageFR[errorCode];
+
+    if (errorMessage) {
+      console.log({ errorMessage });
+      toastShow({ errorMessage });
+    }
+
     // ...
   });
 };
+
+export function toastShow(err) {
+  console.log(err.errorMessage);
+  Toast.show({
+    text: err.errorMessage,
+    buttonText: "Se connecter",
+    buttonTextStyle: { color: "#FFF" },
+    buttonStyle: { backgroundColor: "#5cb85c", borderRadius: 100 },
+    duration: 3000,
+  });
+}
