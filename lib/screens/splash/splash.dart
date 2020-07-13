40cac51465f67aeef4385fbfc1e7bacf0508c64f
lodestar <lodestar.sr@gmail.com>
2020-07-13 23:02:25 +0200

    update splash screen

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -37,19 +37,21 @@ class _SplashState extends State<Splash> {
                       child: Center(
                         child: Text(
                           'Fulltrip',
-                          style: TextStyle(color: AppColors.greyDarkColor, fontSize: 72, fontWeight: FontWeight.bold, shadows: [
-                            BoxShadow(color: AppColors.darkColor.withOpacity(0.25), blurRadius: 4, spreadRadius: 4),
+                          style: TextStyle(color: Colors.white, fontSize: 72, fontWeight: FontWeight.bold, shadows: [
+                            BoxShadow(color: AppColors.darkColor.withOpacity(0.4), blurRadius: 4, spreadRadius: 2, offset: Offset(0, 6)),
                           ]),
                         ),
                       ),
                     ),
                     Container(
+                      margin: EdgeInsets.only(top: 8),
                       child: Center(
                         child: Text(
-                          'Plateforme logistique professionnelle',
-                          style: TextStyle(color: AppColors.greyDarkColor, fontSize: 14, fontWeight: FontWeight.w900, shadows: [
+                          '1ère plateforme d\'échange de lots créée par des déménageurs pour les déménageurs',
+                          style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w900, shadows: [
                             BoxShadow(color: AppColors.darkColor.withOpacity(0.25), blurRadius: 4, spreadRadius: 4),
                           ]),
+                          textAlign: TextAlign.center,
                         ),
                       ),
                     ),
@@ -60,7 +62,7 @@ class _SplashState extends State<Splash> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(30)),
                   boxShadow: <BoxShadow>[
-                    BoxShadow(color: AppColors.whiteColor.withOpacity(0.5), blurRadius: 16, spreadRadius: 4),
+                    BoxShadow(color: AppColors.whiteColor.withOpacity(0.3), blurRadius: 16, spreadRadius: 4),
                   ],
                 ),
                 child: ButtonTheme(
