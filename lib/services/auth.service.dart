0b98ffbf1a985acc441f5c6bdaf1f1ff3bfa0645
lodestar <lodestar.sr@gmail.com>
2020-07-30 01:08:03 +0200

    Update UI

---
+++ lib/services/auth.service.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:Fulltrip/data/models/user.dart';
+import 'package:Fulltrip/util/global.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthService {
+  static AuthService _appService;
+  User loggedInUser;
+
+  static AuthService getInstance() {
+    if (_appService != null) {
+      return _appService;
+    }
+
+    _appService = new AuthService();
+    return _appService;
+  }
+
+  Future updateUser({String key='user', User user}) {
+    if (user != null) {
+      this.loggedInUser = user;
+      String userJson = json.encode(loggedInUser.toJson());
+      return Global.prefs.setString(key, userJson);
+    }
+    return Global.prefs.setString(key, null);
+  }
+
+  User getData({String key='user'}) {
+    String userJson = Global.prefs.getString(key);
+    if (userJson != null) {
+      Map userObj = json.decode(userJson);
+      loggedInUser = User.fromJson(userObj);
+      return loggedInUser;
+    }
+    return null;
+  }
+
+  bool isLoggedIn() {
+    loggedInUser = this.getData(key: 'user');
+    return loggedInUser != null;
+  }
+}
